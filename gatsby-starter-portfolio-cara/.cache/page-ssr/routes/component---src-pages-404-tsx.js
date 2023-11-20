d4dc318a16bd390e82faf63443952b170ebaf1ac
SpringisCalling <Springiscalling@gmail.com>
2023-11-19 23:40:38 -0800

    Test

---
+++ gatsby-starter-portfolio-cara/.cache/page-ssr/routes/component---src-pages-404-tsx.js
@@ -0,0 +1,208 @@
+"use strict";
+exports.id = 218;
+exports.ids = [218];
+exports.modules = {
+
+/***/ 2513:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Head: () => (/* binding */ Head),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8610);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var gatsby__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(7076);
+/* harmony import */ var _react_spring_parallax__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6318);
+/* harmony import */ var _theme_ui_mdx__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(3150);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_components_layout__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(8915);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_elements_divider__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5726);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_styles_animations__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6507);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(6360);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_components_seo__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(9056);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_elements_content__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(4233);
+/* harmony import */ var _lekoarts_gatsby_theme_cara_src_elements_inner__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(1210);
+
+
+
+
+
+
+
+
+
+
+
+const NotFound = _props => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_layout__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_react_spring_parallax__WEBPACK_IMPORTED_MODULE_2__/* .Parallax */ .V, {
+  pages: 1
+}, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_elements_divider__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {
+  speed: 0.2,
+  offset: 0,
+  factor: 1
+}, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_styles_animations__WEBPACK_IMPORTED_MODULE_5__/* .UpDown */ .w6, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "triangle",
+  hiddenMobile: true,
+  width: 48,
+  stroke: true,
+  color: "icon_orange",
+  left: "10%",
+  top: "20%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "hexa",
+  width: 48,
+  stroke: true,
+  color: "icon_red",
+  left: "60%",
+  top: "70%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "box",
+  width: 6,
+  color: "icon_darker",
+  left: "60%",
+  top: "15%"
+})), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_styles_animations__WEBPACK_IMPORTED_MODULE_5__/* .UpDownWide */ .sr, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "arrowUp",
+  hiddenMobile: true,
+  width: 16,
+  color: "icon_blue",
+  left: "80%",
+  top: "10%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "triangle",
+  width: 12,
+  stroke: true,
+  color: "icon_brightest",
+  left: "90%",
+  top: "50%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  width: 16,
+  color: "icon_darker",
+  left: "70%",
+  top: "90%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "triangle",
+  width: 16,
+  stroke: true,
+  color: "icon_darkest",
+  left: "30%",
+  top: "65%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "cross",
+  width: 16,
+  stroke: true,
+  color: "icon_pink",
+  left: "28%",
+  top: "15%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  width: 6,
+  color: "icon_darkest",
+  left: "75%",
+  top: "10%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "upDown",
+  hiddenMobile: true,
+  width: 8,
+  color: "icon_darkest",
+  left: "45%",
+  top: "10%"
+})), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  hiddenMobile: true,
+  width: 24,
+  color: "icon_darker",
+  left: "5%",
+  top: "70%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  width: 6,
+  color: "icon_darkest",
+  left: "4%",
+  top: "20%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  width: 12,
+  color: "icon_darkest",
+  left: "50%",
+  top: "60%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "upDown",
+  width: 8,
+  color: "icon_darkest",
+  left: "95%",
+  top: "90%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "upDown",
+  hiddenMobile: true,
+  width: 24,
+  color: "icon_darker",
+  left: "40%",
+  top: "80%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "triangle",
+  width: 8,
+  stroke: true,
+  color: "icon_darker",
+  left: "25%",
+  top: "5%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "circle",
+  width: 64,
+  color: "icon_green",
+  left: "95%",
+  top: "5%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "box",
+  hiddenMobile: true,
+  width: 64,
+  color: "icon_purple",
+  left: "5%",
+  top: "90%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "box",
+  width: 6,
+  color: "icon_darkest",
+  left: "10%",
+  top: "10%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "box",
+  width: 12,
+  color: "icon_darkest",
+  left: "40%",
+  top: "30%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "hexa",
+  width: 16,
+  stroke: true,
+  color: "icon_darker",
+  left: "10%",
+  top: "50%"
+}), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_svg__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z, {
+  icon: "hexa",
+  width: 8,
+  stroke: true,
+  color: "icon_darker",
+  left: "80%",
+  top: "70%"
+})), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_elements_content__WEBPACK_IMPORTED_MODULE_8__/* ["default"] */ .Z, {
+  sx: {
+    variant: `texts.bigger`
+  },
+  speed: 0.4,
+  offset: 0,
+  factor: 1
+}, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_elements_inner__WEBPACK_IMPORTED_MODULE_9__/* ["default"] */ .Z, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_theme_ui_mdx__WEBPACK_IMPORTED_MODULE_10__/* .Themed */ .Ge.h1, null, "404 - Page not found"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_theme_ui_mdx__WEBPACK_IMPORTED_MODULE_10__/* .Themed */ .Ge.p, null, "Go back to ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link, {
+  to: "/"
+}, "homepage"), "."))))));
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NotFound);
+const Head = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_lekoarts_gatsby_theme_cara_src_components_seo__WEBPACK_IMPORTED_MODULE_7__/* ["default"] */ .Z, {
+  title: "404 - Not Found"
+});
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-404-tsx.js.map
---
+++ gatsby-starter-portfolio-cara/.cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("C:\\Users\\Tripleiii\\Documents\\GitHub\\UETEST\\SpringisCalling.github.io\\gatsby-starter-portfolio-cara\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\Tripleiii\\Documents\\GitHub\\UETEST\\SpringisCalling.github.io\\gatsby-starter-portfolio-cara\\src\\pages\\404.tsx"))
+}
+
